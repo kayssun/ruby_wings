cf93569069d4e63e1f1769b6c647c733c1239056
Gerrit Visscher <gerrit@visscher.de>
2015-03-05 17:50:39 +0100

    [FEATURE] Add blank? method known from Rails

--- lib/RubyWings.rb
+++ lib/RubyWings.rb
@@ -1,5 +1,20 @@
 require 'RubyWings/version'
 
+
+# Make access to hash values easier
+class ::Nil
+  def blank?
+    true
+  end
+end
+
+class ::String
+  def blank?
+    empty?
+  end
+end
+
+
 module RubyWings
   # Your code goes here...
 end
