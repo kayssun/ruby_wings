b573be09911a25fa7ae396381f3928550187d079
Gerrit Visscher <gerrit@visscher.de>
2015-03-08 19:55:02 +0100

    [FEATURE] Add hash deep merge

--- lib/ruby_wings.rb
+++ lib/ruby_wings.rb
@@ -14,6 +14,21 @@ class ::String
   end
 end
 
+# Allow recursive merging of Hashes
+# thanks to http://stackoverflow.com/a/25990044
+class ::Hash
+  def deep_merge(second)
+    merger = proc do |key, v1, v2|
+      if Hash === v1 && Hash === v2
+        v1.merge(v2, &merger)
+      else
+        [:undefined, nil, :nil].include?(v2) ? v1 : v2
+      end
+    end
+    self.merge(second, &merger)
+  end
+
+end
 
 module RubyWings
   # Your code goes here...
